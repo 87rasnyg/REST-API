2c32181b98530ec5dfaccc64f483deea8e259d93
Rasmus Nygren <87rasnyg@skola.boras.se>
2021-08-18 17:09:09 +0200

    added simple post

--- movies.router.js
+++ movies.router.js
@@ -12,4 +12,27 @@ router.get("/api/movies", (req, res) => {
     res.json(movieList);
 });
 
+router.post("/api/movies", (req, res) => {
+    const {title, released, duration} = req.body;
+
+    //get current index
+    if(movieList.length > 0){
+        id = movieList[movieList.length-1].id + 1;
+    }
+    else{
+        id = 0;
+    }
+
+    const movie = {
+        id: id,
+        title: title,
+        released: released,
+        duration: duration
+    }
+
+    movieList.push(movie);
+
+    res.status(201).json(movie);
+});
+
 module.exports = router;
