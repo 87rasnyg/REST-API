c1a5d2caca75049c9e4d207d5be64dee06998ad9
Rasmus Nygren <87rasnyg@skola.boras.se>
2021-08-19 11:43:37 +0200

    added validation

---
+++ movie.validation.js
@@ -0,0 +1,40 @@
+const { Request, Response, NextFunction } = require("express");
+const { body, valdidationResult, validationResult } = require("express-validator");
+
+/**
+ * Checks if any errors occured and sends back a 400 status code if there was
+ * or calls the next function.
+ * @param {Require} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+
+function checkValidation(req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(400).json({errors: errors.array()});
+    }
+    else{
+        next();
+    }
+}
+
+const saveMovieValidation = [
+    body("title").notEmpty(),
+    body("released").notEmpty().isFloat({ min: 1888, max: new Date().getFullYear()}),
+    body("duration").notEmpty().isFloat({min: 1}),
+    checkValidation
+]
+
+const updateMovieValidation = [
+    body("title").exists(),
+    body("released").exists(),
+    body("duration").exists(),
+    checkValidation
+]
+
+module.exports = {
+    saveMovieValidation,
+    updateMovieValidation
+}
--- movies.router.js
+++ movies.router.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const {getMovies, postMovie, updateMovie, deleteMovie} = require("./movie.controllers");
+const { getMovies, postMovie, updateMovie, deleteMovie } = require("./movie.controllers");
+const { saveMovieValidation, updateMovieValidation } = require("./movie.validation");
 
 const router = express.Router();
 
 router.get("/api/movies", getMovies);
-router.post("/api/movies", postMovie);
-router.put("/api/movies/:id", updateMovie);
+router.post("/api/movies", saveMovieValidation, postMovie);
+router.put("/api/movies/:id", updateMovieValidation, updateMovie);
 router.delete("/api/movies/:id", deleteMovie);
 
 module.exports = router;
